5430e8c26c2d26f8e4d1096b8fe3e875edc992b1
Tyler Southwick <tyler@northfuse.net>
2015-06-20 16:32:04 -0700

    fix branch removal for previous branches

--- macros/src/main/scala/unrolling/macros/UnrollFunctions.scala
+++ macros/src/main/scala/unrolling/macros/UnrollFunctions.scala
@@ -1,5 +1,6 @@
 package unrolling.macros
 
+import scala.collection.mutable.ListBuffer
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox
 
@@ -8,14 +9,19 @@ import scala.reflect.macros.blackbox
  */
 object UnrollFunctions {
 
-  def leaf(f: => Unit): Unit = f
-  def branch(f: => Unit): Unit = f
+  def leaf(note : String)(f: => Unit): Unit = f
+  def branch(note : String)(f: => Unit): Unit = f
 
-  def unroll(target : ( () => Unit) => Unit)(f: => Unit): Unit = macro unroll_impl
+  def unroll(target : (Seq[String], () => Unit) => Unit)(f: => Unit): Unit = macro unroll_impl
 
   def unroll_impl(c: blackbox.Context)(target : c.Tree) (f: c.Tree): c.Tree = logIt { log =>
     import c.universe._
     def println(a: Any) = log.println(a)
+
+    implicit class LeafBranchFinder(tree : Tree) {
+      def isBranch = tree.toString().startsWith("unrolling.macros.UnrollFunctions.branch(")
+      def isLeaf = tree.toString().startsWith("unrolling.macros.UnrollFunctions.leaf(")
+    }
 
     val leaves = {
       object nameFinder extends Traverser {
@@ -23,12 +29,17 @@ object UnrollFunctions {
 
         override def traverse(tree: Tree): Unit = tree match {
           case app@Apply(fun, args) =>
-            if ("unrolling.macros.UnrollFunctions.leaf".equals(fun.toString())) {
+            if (fun.isLeaf) {
               applies = app :: applies
-            }
+            } /*else {
+              println("found fun: " + fun.toString() + " [" + showRaw(fun) + "]")
+            }*/
             super.traverse(fun)
             super.traverseTrees(args)
-          case _ => super.traverse(tree)
+          case _ => {
+            //println("found: " + tree.toString() + " [" + showRaw(tree) + "]")
+            super.traverse(tree)
+          }
         }
       }
 
@@ -41,36 +52,76 @@ object UnrollFunctions {
     } yield {
         val otherLeaves = leaves.filterNot(_.equalsStructure(leaf))
         object leafRemover extends Transformer {
-          var found = false
           override def transform(tree: c.universe.Tree): c.universe.Tree = {
-            if (tree == leaf) {
-              found = true
-              leaf
-            } else if (otherLeaves.exists(_.equalsStructure(tree))) {
+            if (otherLeaves.exists(_.equalsStructure(tree))) {
               c.typecheck(reify(null).tree)
             } else {
-              tree match {
-                case app@Apply(fun, args) =>
-                  if (found && "unrolling.macros.UnrollFunctions.branch".equals(fun.toString())) {
-                    c.typecheck(reify(null).tree)
-                  } else {
-                    super.transform(tree)
-                  }
-                case _ => super.transform(tree)
-              }
+              super.transform(tree)
             }
           }
         }
-        leafRemover.transform(f)
+        /**
+         * branches need to be removed in a second pass to handle a case such as
+         *
+         * branch {
+         * }
+         * branch {
+         *   leaf {
+         *   }
+         * }
+         *
+         * Where the first branch needs to be removed
+         */
+        object branchRemover extends Transformer {
+          override def transform(tree: c.universe.Tree): c.universe.Tree = {
+            if (tree.isBranch) {
+              tree match {
+                case app@Apply(Apply(_, _), _) if !app.exists(_.isLeaf)=> {
+                    //println("tree [" + tree + "] has leaf: " + tree.exists(_.isLeaf))
+                    //println("removing tree: " + app + " -> " + app.getClass)
+                    //println("removing tree: " + app.fun + " -> " + app.fun.getClass)
+                    c.typecheck(reify(null).tree)
+                    //tree
+                }
+                case _ => super.transform(tree)
+              }
+            } else {
+              super.transform(tree)
+            }
+          }
+        }
+        val pathWithBranches = leafRemover.transform(f)
+        val path = branchRemover.transform(pathWithBranches)
+        object PathArgBuilder extends Traverser {
+          var args = List[String]()
+
+          override def traverse(tree: c.universe.Tree): Unit = tree match {
+            case app@Apply(fun@Apply(_, funArgs), _) if fun.isBranch || fun.isLeaf => {
+              //println("looking on " + fun + " - " + showRaw(fun))
+              //println("args(" + funArgs.length + ") : " + funArgs)
+              val values = funArgs.flatMap(_.collect({
+                case Literal(Constant(value)) => Option(value).map(_.toString)
+              })).flatten
+              args = args ++ values
+              //println("values: " + values)
+              super.traverse(app)
+            }
+            case _ => super.traverse(tree)
+          }
+        }
+        PathArgBuilder(path)
+        val args = PathArgBuilder.args
+        //println("args: " + args)
+        path -> args
       }
 
-    println("target: " + showRaw(target))
+    //println("target: " + showRaw(target))
     val r = target.collect {
       case Apply(fun, _) => fun
     }
     val applyFun = r.head
-    println("applyFun: " + applyFun)
-    Block(paths.map(path => Apply(applyFun, List(Function(List(), path)))), Literal(Constant()))
+    //println("applyFun: " + applyFun)
+    Block(paths.map(path => Apply(applyFun, List(q"${path._2}", Function(List(), path._1)))), Literal(Constant()))
   }
 
   import java.io._
@@ -78,12 +129,12 @@ object UnrollFunctions {
   def logIt[A](f: PrintWriter => A): A = {
     val out = new PrintWriter(new OutputStreamWriter(new FileOutputStream("/tmp/macro_dev", true)))
     try {
+      out.println("\n" * 55)
+      out.println("generated: " + new java.util.Date)
       f(out)
     } finally {
       out.close()
     }
   }
-
-  case class Node[A](items: Seq[A])
 
 }
--- tests/src/test/scala/unrolling/tests/MacroSpec.scala
+++ tests/src/test/scala/unrolling/tests/MacroSpec.scala
@@ -10,42 +10,60 @@ class MacroSpec extends FunSpec with ShouldMatchers {
 
     var counter = 0
 
-    val buffer = ListBuffer[() => Unit]()
-    def testRegistration( f : () => Unit): Unit = {
-      buffer += f
+    case class RegisteredTest(args : Seq[String], f : () => Unit)
+    val buffer = ListBuffer[RegisteredTest]()
+    def testRegistration(args : Seq[String], f : () => Unit): Unit = {
+      println("found args: " + args)
+      buffer += RegisteredTest(args, f)
     }
 
     import unrolling.macros.UnrollFunctions._
     unroll(testRegistration) {
-      branch {
+      leaf("test1") {
+        counter += 27
+      }
+      branch("branch1") {
         counter += 2
-        leaf {
+        leaf("test1") {
           counter += 1
         }
-        branch {
+        branch("branch2") {
           counter += 3
-          leaf {
+          leaf("test2") {
             counter += 2
           }
-          leaf {
+          leaf("test3") {
             counter += 7
           }
+        }
+      }
+      branch("branch3") {
+        println("branch3")
+        counter += 2
+        leaf("leaf6") {
+          counter += 33
         }
       }
     }
 
     val expected = Seq(
-      3, 7, 12
+      27 -> Seq("test1"),
+      3 -> Seq("branch1", "test1"),
+      7 -> Seq("branch1", "branch2", "test2"),
+      12 -> Seq("branch1", "branch2", "test3"),
+      35 -> Seq("branch3", "leaf6")
     )
     for {
       t <- expected.zipWithIndex
-      value = t._1
+      value = t._1._1
+      path = t._1._2
       index = t._2
     } {
       it(s"should resolve #$index to $value") {
-        val f = buffer(index)
+        val registeredTest = buffer(index)
         counter = 0
-        f.apply()
+        registeredTest.f.apply()
+        registeredTest.args should equal(path)
         counter should equal(value)
       }
     }
